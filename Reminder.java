bb7f0aa21f5e1008e1cdfaf924c1f49c4fc4c7c3
Charlie Jung <32102793+Charlanx@users.noreply.github.com>
2018-05-27 17:27:05 -0600

    removed comments

--- Reminder.java
+++ Reminder.java
@@ -13,76 +13,71 @@ import java.util.Calendar;
 import java.util.Date;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-	
+
 public class Reminder implements KeyListener {
 
-		static String text = "";
-		static String finalText = "";
-		static JLabel label = new JLabel(text, JLabel.CENTER);
-		static boolean isRunning = false;
-		static FileWriter fw;
-		static BufferedWriter bw;
-		static Font font;
-		static int screenWidth;
-		static int screenHeight;
-		static String desktopLocationString;
-		static Calendar calendar;
-        	static FileWriter fw2;
-        	static BufferedWriter bw2;
-		public static void main(String[] args) throws IOException {
-			Dimension screenDimensions = Toolkit.getDefaultToolkit().getScreenSize();
-	        	screenWidth = (int) screenDimensions.getWidth();
-	        	screenHeight = (int) screenDimensions.getHeight();
-			JFrame frame = new JFrame();
-			font = new Font("TimesNewRoman", Font.PLAIN, 100);
-			label.setFont(font);
-			label.setText("Write your Daily Reminder for Today."); 
-	        	frame.setTitle("Daily Reminder Application");
-			frame.addKeyListener(new Reminder());
-			frame.add(label);
-			frame.setSize(screenWidth, screenHeight); 
-			frame.setVisible(true); 
-			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			desktopLocationString = System.getProperty("user.home") + "/Desktop";
-			//File desktopLocation = new File(desktopLocationString, "test.txt");
-			//fw = new FileWriter(desktopLocation);
-		   	//bw = new BufferedWriter(fw);
-		    	calendar = Calendar.getInstance();
-		    	frame.addWindowListener (new java.awt.event.WindowAdapter() {
-		        
-		    @Override
-		    public void windowClosing(java.awt.event.WindowEvent windowEvent) {
-                    	try {
-                    	    finalText = text;
-                            File dailyRemindersFile = new File(desktopLocationString + "/DailyReminders");
-                            if (dailyRemindersFile.exists() == false) {
-                            	dailyRemindersFile.mkdir();
-                            } else if (dailyRemindersFile.exists() == true) {
-                    }
-                    String dailyRemindersPath = desktopLocationString + "\\DailyReminders";
-                    Date date = calendar.getTime();
-                    SimpleDateFormat formattedDate = new SimpleDateFormat("MM-dd-YYYY");
-                    String currentDate = formattedDate.format(date);
-                    File dailyRemindersLocation = new File(dailyRemindersPath, "\\DailyReminders" + currentDate + ".txt");
-                    fw2 = new FileWriter(dailyRemindersLocation, true);
-                    bw2 = new BufferedWriter(fw2);
-                    if (dailyRemindersLocation.exists() == true) {
-                        bw2.append(finalText + "\n");
-                    }else {
-                        //System.out.println("first");
-                        // bw2.write(finalText + "\n");
-                    }
-                    //bw.flush();
-                    //bw.close();
-                    bw2.flush();
-                    bw2.close();
-                } catch (IOException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
-		    }});
-		}
-		       
+	static String text = "";
+	static String finalText = "";
+	static JLabel label = new JLabel(text, JLabel.CENTER);
+	static boolean isRunning = false;
+	static FileWriter fw;
+	static BufferedWriter bw;
+	static Font font;
+	static int screenWidth;
+	static int screenHeight;
+	static String desktopLocationString;
+	static Calendar calendar;
+	static FileWriter fw2;
+	static BufferedWriter bw2;
+
+	public static void main(String[] args) throws IOException {
+		Dimension screenDimensions = Toolkit.getDefaultToolkit().getScreenSize();
+		screenWidth = (int) screenDimensions.getWidth();
+		screenHeight = (int) screenDimensions.getHeight();
+		JFrame frame = new JFrame();
+		font = new Font("TimesNewRoman", Font.PLAIN, 100);
+		label.setFont(font);
+		label.setText("Write your Daily Reminder for Today.");
+		frame.setTitle("Daily Reminder Application");
+		frame.addKeyListener(new Reminder());
+		frame.add(label);
+		frame.setSize(screenWidth, screenHeight);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		desktopLocationString = System.getProperty("user.home") + "/Desktop";
+		calendar = Calendar.getInstance();
+		frame.addWindowListener(new java.awt.event.WindowAdapter() {
+
+			@Override
+			public void windowClosing(java.awt.event.WindowEvent windowEvent) {
+				try {
+					finalText = text;
+					File dailyRemindersFile = new File(desktopLocationString + "/DailyReminders");
+					if (dailyRemindersFile.exists() == false) {
+						dailyRemindersFile.mkdir();
+					} else if (dailyRemindersFile.exists() == true) {
+					}
+					String dailyRemindersPath = desktopLocationString + "\\DailyReminders";
+					Date date = calendar.getTime();
+					SimpleDateFormat formattedDate = new SimpleDateFormat("MM-dd-YYYY");
+					String currentDate = formattedDate.format(date);
+					File dailyRemindersLocation = new File(dailyRemindersPath,
+							"\\DailyReminders" + currentDate + ".txt");
+					fw2 = new FileWriter(dailyRemindersLocation, true);
+					bw2 = new BufferedWriter(fw2);
+					if (dailyRemindersLocation.exists() == true) {
+						bw2.append(finalText + "\n");
+					}
+					bw2.flush();
+					bw2.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
 	@Override
 	public void keyTyped(KeyEvent e) {
 		if (e.getID() == KeyEvent.KEY_TYPED) {
@@ -101,4 +96,4 @@ public class Reminder implements KeyListener {
 	public void keyReleased(KeyEvent e) {
 
 	}
-	}
+}
